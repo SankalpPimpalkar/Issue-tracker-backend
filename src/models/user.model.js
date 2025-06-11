23da4eacef598f0664acd8de25604bc2f1ea81b6
SankalpPimpalkar <pimpalkarsankalp8@gmail.com>
2025-06-11 13:14:19 +0530

    added avatar field in user model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -12,6 +12,10 @@ const userSchema = new mongoose.Schema({
         required: true,
         unique: true
     },
+    avatar: {
+        type: String,
+        default: "https://png.pngtree.com/png-clipart/20200224/original/pngtree-cartoon-color-simple-male-avatar-png-image_5230557.jpg"
+    },
     password: {
         type: String,
         required: true,
